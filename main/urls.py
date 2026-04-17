b1dcba3167f2125abd5eb50238619095764bfc65
granzular <ayenimichael92@gmail.com>
2026-04-17 01:16:47 +0100

    disabled trailing slash in urls. removed browsable api renderer

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 app_name = 'main'
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'profiles',views.ProfileViewSet,basename='create-list-profile')
 router.register(r'profiles/<pk>',views.ProfileViewSet,basename='detail-delete-profile')
 urlpatterns = router.urls
--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ REST_FRAMEWORK = {
         'DEFAULT_PARSER_CLASSES': [
             'rest_framework.parsers.JSONParser'
             ],
-        'DEFAULT_RENDERER_CLASSES':['rest_framework.renderers.JSONRenderer','rest_framework.renderers.BrowsableAPIRenderer'],
+        'DEFAULT_RENDERER_CLASSES':['rest_framework.renderers.JSONRenderer',],
 
         }
 
